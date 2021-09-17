b5bfa801d1679eb11be7e2eab3efeed66efd03d6
Jasp197 <jaspreet197197@gmail.com>
2021-09-17 14:08:55 -0500

    commit crud

--- src/main/java/com/ex/dockerspringboothello/DockerSpringbootHelloApplication.java
+++ src/main/java/com/ex/dockerspringboothello/DockerSpringbootHelloApplication.java
@@ -2,8 +2,10 @@ package com.ex.dockerspringboothello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ex")
 public class DockerSpringbootHelloApplication {
 
 	public static void main(String[] args) {
